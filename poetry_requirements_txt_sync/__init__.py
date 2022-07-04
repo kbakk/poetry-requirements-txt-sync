dcc605f007939f5f1645b9d93b0f37a00066bb9a
Kristoffer Bakkejord <kbakk@users.noreply.github.com>
2022-07-04 17:21:58 +0200

    Initial commit

---
+++ poetry_requirements_txt_sync/__init__.py
@@ -0,0 +1,67 @@
+import pathlib
+import subprocess
+import sys
+from typing import List, Optional
+
+__version__ = "0.1.0"
+
+
+def execute_sync_command(
+    extras_name: str, requirements_txt_path: pathlib.Path, hashes=False
+):
+    cmd = (
+        f"poetry export --format=requirements.txt "
+        f"--extras {extras_name} "
+        f"--output {requirements_txt_path}"
+    )
+    if hashes:
+        cmd += " --without-hashes"
+    print(cmd)
+    run_res = subprocess.run(cmd.split())
+    run_res.check_returncode()
+    return
+
+
+def cli():
+    import typer
+
+    def version_callback(value: bool):
+        if value:
+            typer.echo(f"{__version__}")
+            raise typer.Exit()
+
+    def mappings_callback(mappings: List[str]):
+        for mapping in mappings:
+            if len([i for i, c in enumerate(mapping) if c == ":"]) != 1:
+                raise typer.BadParameter(
+                    f"Bad format of {mapping=!r}, expected to find a single ':'"
+                )
+        return mappings
+
+    def _cli(
+        mappings: List[str] = typer.Argument(..., callback=mappings_callback),
+        _: Optional[bool] = typer.Option(
+            None, "--version", callback=version_callback, is_eager=True
+        ),
+    ):
+        exit_code = 0
+        for extras_name, requirements_txt_path in (m.split(":") for m in mappings):
+            try:
+                execute_sync_command(
+                    extras_name=extras_name,
+                    requirements_txt_path=pathlib.Path(requirements_txt_path),
+                )
+            except subprocess.CalledProcessError as e:
+                program = e.args[1][0]
+                exit_code = e.args[0]
+                typer.echo(
+                    f"{program} exited with exit code {exit_code}, check output",
+                    err=True,
+                )
+        sys.exit(0)
+
+    typer.run(_cli)
+
+
+if __name__ == "__main__":
+    cli()
